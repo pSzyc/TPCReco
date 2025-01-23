7d4664912d09629b59e4aee35c1086f3d61685e4
p.szyc <p.szyc@omniviser.ai>
2025-01-23 18:27:50 +0100

    feat: proper model and tf versions

--- Tensorflow/create_model.py
+++ Tensorflow/create_model.py
@@ -1,13 +1,25 @@
 # Imports
 import tensorflow as tf
 
-input_1 = tf.keras.Input(shape=(5,))
+model = tf.keras.Sequential([
+  tf.keras.layers.Resizing(height=128, width=128), 
+  tf.keras.layers.Conv2D(16, 5, padding='same', activation='relu', 
+                         data_format="channels_last"),
+  tf.keras.layers.MaxPooling2D(),
+  tf.keras.layers.Conv2D(32, 5, padding='same', activation='relu', 
+                         data_format="channels_last"),
+  tf.keras.layers.MaxPooling2D(),
+  tf.keras.layers.Conv2D(64, 5, padding='same', activation='relu', 
+                         data_format="channels_last"),
+  tf.keras.layers.MaxPooling2D(),
+  tf.keras.layers.Flatten(),
+  tf.keras.layers.Dense(128, activation='relu'),
+  tf.keras.layers.Dense(128, activation='relu'), 
+  tf.keras.layers.Dense(128, activation='relu'),
+  tf.keras.layers.Dense(9)
+])
 
-output_1 = tf.keras.layers.Dense(5, activation=tf.nn.relu)(input_1)
-output_1 = tf.keras.layers.Dense(1, activation=tf.nn.sigmoid)(output_1)
-model = tf.keras.Model(inputs=input_1, outputs=output_1)
-
-model.compile()
+model.build(input_shape=(None, 256, 512, 3))
 
 # Export the model to a SavedModel
-model.save('model', save_format='tf')
+model.export('model-test')
--- Tensorflow/main.cpp
+++ Tensorflow/main.cpp
@@ -2,58 +2,76 @@
 #include <iostream>
 #include "tf_functions.h"
 
+// just for tests, please delete it later
+std::vector<std::vector<std::vector<std::vector<int>>>> createVectorOfOnes(int batch_size) {
+    const int height = 256;
+    const int width = 512;
+    const int channels = 3;
+
+    // Create a 4D vector filled with ones
+    std::vector<std::vector<std::vector<std::vector<int>>>> vec(
+        batch_size, 
+        std::vector<std::vector<std::vector<int>>>(
+            height, 
+            std::vector<std::vector<int>>(
+                width, 
+                std::vector<int>(channels, 1)
+            )
+        )
+    );
+
+    return vec;
+}
+
 int main () {
-	std::vector<float> x_test_0 = {1, 2, 3, 4, 5};
-	std::vector<float> y_test_0 = {1};
+    int batch_size = 1; // Example batch size
+    auto x_test = createVectorOfOnes(batch_size);
 
+    char model_path[] = "../model-test";
+    TF_Graph * graph = nullptr;
+    TF_Session * session = nullptr;
+    TF_Tensor * input_tensor = nullptr, * output_tensor = nullptr;
 
-	char model_path[] = "../model";
-	TF_Graph * graph = nullptr;
-	TF_Session * session = nullptr;
-	TF_Tensor * input_tensor = nullptr, * output_tensor = nullptr;
+    tf_functions::load_session(model_path, &graph, &session);
 
-	tf_functions::load_session(model_path, &graph, &session);
+    TF_Operation * input_op = TF_GraphOperationByName(graph, "serve_keras_tensor");
+    TF_Output input = TF_Output{input_op, 0};
+    if (input.oper == nullptr) {
+        std::cout << "Can't init input_op" << std::endl;
+        return 2;
+    }
 
-	TF_Operation * input_op = TF_GraphOperationByName(graph, "serving_default_input_1");
-	TF_Output input = TF_Output{input_op, 0};
-	if (input.oper == nullptr) {
-		std::cout << "Can't init input_op" << std::endl;
-		return 2;
-	}
+    std::cout << "init input_op" << std::endl;
 
-	std::cout << "init input_op" << std::endl;
+    TF_Operation * output_op = TF_GraphOperationByName(graph, "StatefulPartitionedCall");
+    TF_Output output = TF_Output{output_op, 0};
+    if (output.oper == nullptr) {
+        std::cout << "Can't init output_op" << std::endl;
+        return 2;
+    }
 
-	TF_Operation * output_op = TF_GraphOperationByName(graph, "StatefulPartitionedCall");
-	TF_Output output = TF_Output{output_op, 0};
-	if (output.oper == nullptr) {
-		std::cout << "Can't init output_op" << std::endl;
-		return 2;
-	}
+    std::cout << "init output_op" << std::endl;
 
-	std::cout << "init output_op" << std::endl;
+    std::vector<std::int64_t> dims = {batch_size, 256, 512, 3};
+    int num_dims = 4;
+    tf_functions::create_tensor(TF_FLOAT, dims, num_dims, x_test, &input_tensor);
 
+    tf_functions::run_session(session,
+        &input, &input_tensor, 1,
+        &output, &output_tensor, 1);
 
-	std::vector<std::int64_t> dims = {1, 5};
-	int num_dims = 2;
-	tf_functions::create_tensor(TF_FLOAT, dims, num_dims, x_test_0, &input_tensor);
+    // Check results
+    auto tensor_data = static_cast<float*>(TF_TensorData(output_tensor));
 
-	tf_functions::run_session (session,
-		&input, &input_tensor, 1,
-		&output, &output_tensor, 1);
+    std::cout << std::endl << "Prediction:" << std::endl;
 
-
-	// Check results
-	auto tensor_data = static_cast<float*>(TF_TensorData(output_tensor));
-
-	std::cout << std::endl << "Prediction:" << std::endl;
-
-	for (int i = 0; i < 1; i++) {
+    for (int i = 0; i < 9; i++) {
         std::cout << tensor_data[i] << std::endl;
     }
-	tf_functions::delete_tensor(input_tensor);
-	tf_functions::delete_tensor(output_tensor);
-	tf_functions::delete_graph(graph);
-	tf_functions::delete_session(session);
+    tf_functions::delete_tensor(input_tensor);
+    tf_functions::delete_tensor(output_tensor);
+    tf_functions::delete_graph(graph);
+    tf_functions::delete_session(session);
 
-	return 0;
+    return 0;
 }
